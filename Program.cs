3b0d492b08b1367587109d5fc62b0d8d0fa3ab8d
hom.lamsal500@gmail.com <hom.lamsal500@gmail.com>
2019-08-08 16:47:43 -0500

    c

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace hm1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+
+
+            {
+                int firstno;
+                int secondno;
+                int sumVal;
+                int subVal;
+                int multVal;
+                double divVal;
+                
+
+                Console.WriteLine("Enter first no :");
+                firstno = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter second no: ");
+                secondno = Convert.ToInt32(Console.ReadLine());
+                sumVal = firstno + secondno;
+                Console.WriteLine("Addition of two no is: " + sumVal);
+                subVal = firstno - secondno;
+                Console.WriteLine("Substraction of two no is: " + subVal);
+                multVal = firstno * secondno;
+                Console.WriteLine("Multiplication  of two no is: " + multVal);
+                divVal = (double)firstno / secondno;
+                Console.WriteLine("Division  of two no is: " + divVal);
+                
+
+            }
+        }
+    }
+}
